323b64ab874d4bbc2b11999688ed6153d79ab0d5
MaksymV17 <94906524+MaksymV17@users.noreply.github.com>
2024-10-26 21:50:34 +0300

    Deletes-files
    because I creating new branch

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { initModalEvents } from './modules/Modal.js'; 
-import { initScrollEvents } from './modules/Scroll.js';
-import { fetchData } from './modules/Fetch.js';
-
-initModalEvents(); // Ініціалізація подій для модального вікна.
-initScrollEvents(); // Ініціалізація подій для прокрутки.
-fetchData(); // Виклик функції для отримання даних.
